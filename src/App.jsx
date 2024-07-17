91626c5a579a8d3db61bb5517148df0384d57bc3
Nikita Anatolievich Dudnik <nikdudnik@posteo.com>
2024-07-17 17:58:34 +0200

    Improved REGL setup and frame rendering with better camera and projection matrix handling.

--- src/App.jsx
+++ src/App.jsx
@@ -23,39 +23,53 @@ const savedState = parseState(window.location.hash) || {
 };
 
 const seed = signal(MIN_SEED);
-const random = signal(seedrandom(seed.value.toString()));
-const regl = signal(undefined)
+
+let regl = (createREGL({
+    canvas,
+    attributes: {
+        antialias: true,
+        samples: 4  // This enables 2x MSAA (4 samples)
+    }
+}))
+function resetRegl() {
+    regl.destroy();
+    regl = createREGL({
+        canvas,
+        attributes: {
+            antialias: true,
+            samples: 4  // This enables 2x MSAA (4 samples)
+        }
+    })
+}
+
+
 const numBatches = signal(MIN_BATCHES);
 const numGroups = signal(MIN_GROUPS);
 
 const cameraZPosition = signal(15); // New variable for camera Z position
 const aspectRatio = signal(1);
 
-const projectionMatrix = computed(() =>  mat4.perspective([], Math.PI / 6, aspectRatio.value, 0.01, 1000));
+const projectionMatrix = computed(() => mat4.perspective([], Math.PI / 6, aspectRatio.value, 0.01, 1000));
 const viewMatrix = computed(() => mat4.lookAt([], [0, 0, cameraZPosition.value], [0, 0, 0], [0, 1, 0]));
 
+
 effect(() => {
-    random.value = seedrandom(seed.value.toString());
-    regl.value = createREGL({
-        canvas: canvas,
-        attributes: {
-            antialias: true,
-            samples: 4  // This enables 2x MSAA (4 samples)
-        }
-    })
-    upateReglFrameCB(regl.value, numBatches.value, numGroups.value, random.value);
-    regl.value.poll();
+    resetRegl();
+    setReglFrame(regl, numBatches.value, numGroups.value, seedrandom(seed.value.toString()));
+    regl.poll();
 })
 
-function resize({innerWidth, innerHeight}) {
+function resize({ innerWidth, innerHeight }) {
     canvas.width = innerWidth;
     canvas.height = innerHeight;
     aspectRatio.value = innerWidth / innerHeight;
 }
 
-effect(()=> {
+effect(() => {
+    viewMatrix.value;
+    projectionMatrix.value;
     aspectRatio.value;
-    regl.value.poll();
+    regl.poll();
 })
 
 window.addEventListener('resize', () => resize(window), false)
@@ -86,7 +100,7 @@ function parseState(hash) {
 }
 
 
-function upateReglFrameCB(regl, numBatches, numGroups, random) {
+function setReglFrame(regl = regl.value, numBatches = numBatches.value, numGroups = numGroups.value, random = random.value, view = viewMatrix.value, projection = projectionMatrix.value) {
     const lineBatches = createLineBatches(regl, numBatches, numGroups, random);
     const startTime = Date.now();
     regl.frame(() => {
@@ -95,17 +109,16 @@ function upateReglFrameCB(regl, numBatches, numGroups, random) {
             depth: 1
         });
 
-        const currentTime = (Date.now() - startTime) / 1000; // Convert to seconds
+        const time = (Date.now() - startTime) / 1000; // Convert to seconds
 
         const uniforms = {
-            view: viewMatrix.value,
-            projection: projectionMatrix.value,
-            time: currentTime
+            view,
+            projection,
+            time
         };
 
         lineBatches.forEach(batch => batch.draw(uniforms));
     });
-    regl.poll();
 }
 
 
@@ -113,19 +126,15 @@ export function init() {
     const App = <div>
         <label>Lines</label>
         <group> <label>Batches</label>
-            <input type='number' value={numBatches.value} min={MIN_BATCHES} max={MAX_BATCHES} onChange={(e) => { numBatches.value = parseInt(e.currentTarget.value, 10) }} />
+            <input type='number' value={numBatches.value} min={MIN_BATCHES} max={MAX_BATCHES} onChange={(e) => { numBatches.value = parseInt(e.currentTarget.value, 10);  }} />
             <label>Groups</label>
-            <input type='number' value={numGroups.value} min={MIN_GROUPS} max={MAX_GROUPS} onChange={(e) => { numGroups.value = parseInt(e.currentTarget.value, 10) }} />
+            <input type='number' value={numGroups.value} min={MIN_GROUPS} max={MAX_GROUPS} onChange={(e) => { numGroups.value = parseInt(e.currentTarget.value, 10); }} />
         </group>
         <label>Random Seed</label>
-        <input type='number' value={seed.value} min={MIN_SEED} onChange={(e) => { seed.value = parseInt(e.currentTarget.value, 10) }} />
+        <input type='number' value={seed.value} min={MIN_SEED} onChange={(e) => { seed.value = parseInt(e.currentTarget.value, 10); }} />
 
     </div>;
     render(App, uiContainer);
-}
-
-function updateViewport(regl) {
-    regl.poll();
 }
 
 function createLineBatches(regl, numBatches, groupsNum, random) {
